a5b2cc0aab66c390e23708e1a8ed5d37f22a1e53
glonetechdeveloper <princejosephtimi5@gmail.com>
2025-09-14 22:46:38 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -15,3 +15,55 @@ document.querySelectorAll(".sidebar a").forEach(link => {
     sidebar.classList.remove("active");
   });
 });
+
+
+
+
+// Open modal
+function openModal(imgElement) {
+  var modal = document.getElementById("imgModal");
+  var modalImg = document.getElementById("modalImg");
+
+  modal.style.display = "block";
+  modalImg.src = imgElement.src;
+}
+
+// Close modal
+function closeModal() {
+  document.getElementById("imgModal").style.display = "none";
+}
+
+// Close modal with ESC key
+document.addEventListener("keydown", function(event) {
+  if (event.key === "Escape") {
+    closeModal();
+  }
+});
+
+
+
+
+
+
+
+
+// Open modal
+function openModal(imgElement) {
+  var modal = document.getElementById("imgModal");
+  var modalImg = document.getElementById("modalImg");
+
+  modal.style.display = "block";
+  modalImg.src = imgElement.src;
+}
+
+// Close modal
+function closeModal() {
+  document.getElementById("imgModal").style.display = "none";
+}
+
+// ESC to close modal
+document.addEventListener("keydown", function(event) {
+  if (event.key === "Escape") {
+    closeModal();
+  }
+});
